27b93dfd57d414288e7d6d0b2de0c5690f8bc7d4
armentanoc <armentanocarolina@gmail.com>
2024-01-12 07:46:48 -0300

    feat: testing address in program.

--- AdaTech.AdaShop/AdaTech.AdaShop.ApresentationConsole/Program.cs
+++ AdaTech.AdaShop/AdaTech.AdaShop.ApresentationConsole/Program.cs
@@ -9,13 +9,27 @@
         static void Main(string[] args)
         {
             //Polimorfismo
-            ShippingCompany correiosShippingCompany = new CorreiosShippingCompany();
-            ShippingCompany uberShippingCompany = new UberShippingCompany();
 
-            var order1 = new OrderAggregate(customer: null, shippingCompany: correiosShippingCompany);
-            var order2 = new OrderAggregate(customer: null, shippingCompany: uberShippingCompany);
+            try
+            {
+                ShippingCompany correiosShippingCompany = new CorreiosShippingCompany();
+                ShippingCompany uberShippingCompany = new UberShippingCompany();
 
-            Console.WriteLine("Hello, World!");
+                var order1 = new OrderAggregate(customer: null, shippingCompany: correiosShippingCompany);
+                var order2 = new OrderAggregate(customer: null, shippingCompany: uberShippingCompany);
+
+                var address1 = new Address("Rua1", "2", "Casa", "Bairro1", "Cidade1", "Estado1", "40280000");
+
+                //var address2 = new Address("Rua1", "2", "Casa", "Bairro1", "Cidade1", "Estado1", "4028000"); 
+                    //Output: O cep deve ter 8 caracteres.
+                //var address3 = new Address("Rua1", "2", "Casa", "Bairro1", "Cidade1", "Estado1", "40-80000"); 
+                    //Output:O cep só pode ser composto por algarismos numéricos.
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
