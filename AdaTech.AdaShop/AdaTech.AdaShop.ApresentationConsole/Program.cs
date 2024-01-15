b048fec3eeb15b19dbddafd887a4cbaa586d2d29
Fábio Fonseca <fabio.fonseca@biti9.com.br>
2024-01-15 17:52:37 -0300

    Add Ex. Create Customer

--- AdaTech.AdaShop/AdaTech.AdaShop.ApresentationConsole/Program.cs
+++ AdaTech.AdaShop/AdaTech.AdaShop.ApresentationConsole/Program.cs
@@ -1,12 +1,25 @@
 ﻿namespace AdaTech.AdaShop.ApresentationConsole
 {
     using AdaTech.AdaShop.Domain.Contracts;
+    using AdaTech.AdaShop.Domain.Models;
     using AdaTech.AdaShop.Domain.Models.Order;
     using AdaTech.AdaShop.Domain.Models.ShippingCompany;
+    using AdaTech.AdaShop.Infra.Data.Repository;
 
     internal class Program
     {
         static void Main(string[] args)
+        {
+            //DDD
+            //separation of Concern
+
+            var customer = new Customer("Fábio");
+            var repository = new RepositoryBase<Customer>();
+
+            repository.Insert(customer);
+        }
+
+        void Aula2()
         {
             //Polimorfismo
             ShippingCompany correiosShippingCompany = new CorreiosShippingCompany();
